d94c71378be1ec217fd0cca86c58a1407fd7f7b9
Pranav <pranavgambhire9890@gmail.com>
2026-02-06 23:15:53 +0530

    console log for DB;

--- Backend/Config/DBConnect.js
+++ Backend/Config/DBConnect.js
@@ -4,10 +4,10 @@ require("dotenv").config();
 exports.connectDB = () => {
     mongoose.connect(process.env.MONGODB_URL)
     .then(() => {
-        // console.log("DB connected successfully");
+        console.log("DB connected successfully");
     })
     .catch((err) => {
-        // console.log("DB connection issue");
-        // console.log(err);
+        console.log("DB connection issue");
+        console.log(err);
     });
 }
